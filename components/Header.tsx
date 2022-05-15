01e7f7414a07df6a36554605153f8975f3b98c5d
zaritzki <zars195@gmail.com>
2022-05-15 16:37:54 +0100

    init commit with header

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { SearchIcon, BellIcon } from '@heroicons/react/solid'
+
+
+const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+	return (
+		<header className={`${isScrolled && 'bg-[#141414]'}`}>
+            <div className="flex item-centered space-x-2 md:space-x-10">
+                <img
+                    src="zeeflix.svg"
+                    width={100}
+                    height={50}
+                    className="cursor-pointer object-contain"
+                />
+
+                <ul className="hidden space-x-4 md:flex">
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">TV Shows</li>
+                    <li className="headerLink">Movies</li>
+                    <li className="headerLink">New &amp; Popular</li>
+                    <li className="headerLink">My List</li>
+                </ul>
+            </div>
+
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <SearchIcon className="hidden h-6 w-6 sm:inline" />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon className="h-6 w-6" />
+                <Link href="/account">
+                    <img
+                        src="https://rb.gy/g1pwyx"
+                        alt=""
+                        className="cursor-pointer rounded"
+                    />
+                </Link>
+            </div>
+        </header>
+	)
+}
+
+export default Header
